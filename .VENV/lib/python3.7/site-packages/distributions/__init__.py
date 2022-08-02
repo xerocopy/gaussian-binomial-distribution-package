f34408c23915fb973ed801c66b0352daf98e8944
EC2 Default User <ec2-user@ip-172-31-72-191.ec2.internal>
2022-08-02 03:40:20 +0000

    new project to push to GitHub

---
+++ .VENV/lib/python3.7/site-packages/distributions/__init__.py
@@ -0,0 +1,3 @@
+from .Generaldistribution import Distribution
+from .Gaussiandistribution import Gaussian
+from .Binomialdistribution import Binomial
---
+++ 3a_python_package/build/lib/distributions/__init__.py
@@ -0,0 +1,1 @@
+from .Generaldistribution import Distribution
